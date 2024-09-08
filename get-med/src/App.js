47de5663405ce5a8f3be6d96097f52f7021bea6d
José Calfiman <134818293+JoseCalfiman@users.noreply.github.com>
2024-09-08 15:48:26 -0300

    Se agrego un calendario a la interfaz
    Para su mejor visualización se utiliza de momento su css predeterminado

--- get-med/src/App.js
+++ get-med/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useState } from 'react'; 
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './Calendario.css'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [date, setDate]= useState(new Date());
+  const handleDateChange= (newDate) => {
+    setDate(newDate);
+    console.log('Fecha escogida:', newDate);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Libreria de calendario adecuada "React Calendar"</h1>
+        <Calendar onChange={handleDateChange} value={date}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
